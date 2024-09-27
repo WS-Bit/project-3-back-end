962c713c88dd9bb2b448163731264fba667e23de
wmsexton <wmsexton@btinternet.com>
2024-09-27 10:42:31 +0100

    changed cors

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -9,9 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL
-}));
+app.use(cors())
 app.use('/api', router);
 
 // Start the server
